34d663819dfa3b4f8d724a37077e5c1d78dce333
dummy99z <47759515+dummy99z@users.noreply.github.com>
2022-01-21 18:58:37 -0600

    Update WeatherForecastController.cs
    add ] faltaba en  final line 11

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 namespace MyMicroservice.Controllers
 {
     [ApiController]
-    [Route("[controller]")
+    [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
